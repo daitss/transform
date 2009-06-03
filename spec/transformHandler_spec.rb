c41a1e933073c4edd0fd11872b697aba16634425
Carol <Carol@e2ae85e3-cf20-0410-855b-be2d16f42433>
2009-06-03 17:42:32 +0000

    sinatra based
    git-svn-id: svn://tupelo.fcla.edu/daitss2/transform/trunk@1873 e2ae85e3-cf20-0410-855b-be2d16f42433

--- spec/transformHandler_spec.rb
+++ spec/transformHandler_spec.rb
@@ -1,141 +1,59 @@
-require 'transformHandler'
-require 'net/http'
+require 'transform'
+require 'spec'
+require 'spec/interop/test'
+require 'sinatra/test'
 
-describe TransformHandler do
+describe 'Transform Service' do
+  include Sinatra::Test
 
   before(:all) do
-    begin
-      @server = Mongrel::HttpServer.new("localhost", "3003")
-    rescue => ex
-      STDERR.puts "Can't start server on #{MHOST}:#{PORT}:  #{ex.message}"
-      exit    
+      @url = '/transform'
     end
-    #create and register the describe handler
-    @transformhandler = TransformHandler.new('/var/tmp/transform/')
-    @server.register("/transform", @transformhandler)    
-    @url = 'http://localhost:3003/transform/'
-    STDERR.puts "server started"
-    @server.run
-  end
-
-  after(:all) do
-    @server.stop
-    STDERR.puts "server stoped"
-  end
-
-  def send(method, querystring = nil, data=nil, head=nil)
-    url = URI.parse(@url)   
-    #puts querystring
-    response = Net::HTTP.start(url.host, url.port) {|http|
-      unless (querystring.nil?)
-        http.send_request(method, url.path + querystring, data, head)
-      else
-        http.send_request(method, url.path, data, head)
-      end
-    }
-    response
-  end
 
   it "should return 200 for a successful GET on WAVE_NORM transformation information" do
-    querystring = "WAVE_NORM"
-    response = send("GET", querystring)
+    get "transform/WAVE_NORM"
     puts response.body
-    response.code.to_i.should == 200
+    response.code.should be_ok
   end
 
-  #TODO: revisit
   it "should return 501 for a GET on an invalid transformation" do
-    querystring = "BOGUS_NORM"
-    response = send("GET", querystring)
-    puts response.body
+    get "transform/BOGUS_NORM"
     response.code.to_i.should == 501
   end
 
   it "should return 200 for a successful GET on WAVE_NORM" do
-    querystring = "WAVE_NORM?location=file:///Users/Carol/Workspace/describe/files/GLASS.WAV"
-    response = send("GET", querystring)
-    #puts response.body
-    response.code.to_i.should == 200
+    get "transform/WAVE_NORM?location=file:///Users/Carol/Workspace/describe/files/GLASS.WAV"
+    response.should be_ok
   end
 
   it "should return 200 for a successful GET on PDF_NORM" do
-    querystring = "PDF_NORM?location=file:///Users/Carol/Desktop/work/testdata/pdf/movietest.pdf"
-    response = send("GET", querystring)
-    #puts response.body
-    response.code.to_i.should == 200
+    get "transform/PDF_NORM?location=file:///Users/Carol/Desktop/work/testdata/pdf/movietest.pdf"
+    response.should be_ok
   end
 
   it "should return 200 for a successful GET on MOV_NORM" do
-    querystring = "MOV_NORM?location=file:///Users/Carol/Desktop/work/testdata/video/mov/mama.mov"
-    response = send("GET", querystring)
-    #puts response.body
-    response.code.to_i.should == 200
+    qet "transform/MOV_NORM?location=file:///Users/Carol/Desktop/work/testdata/video/mov/mama.mov"
+    response.should be_ok
   end
 
   it "should return 200 for a successful GET on AVI_NORM" do
-    querystring = "AVI_NORM?location=file:///Users/Carol/Desktop/work/testdata/video/avi/video.avi"
-    response = send("GET", querystring)
-    #puts response.body
-    response.code.to_i.should == 200
+    get "transform/AVI_NORM?location=file:///Users/Carol/Desktop/work/testdata/video/avi/video.avi"
+    response.code.to_i.should be_ok
   end
 
   it "should return 404 for file not found" do
-    querystring = "WAVE_NORM?location=file:///Users/Carol/Workspace/describe/files/GLA"
-    response = send("GET", querystring)
-    puts response.body
+    get "transform/WAVE_NORM?location=file:///Users/Carol/Workspace/describe/files/GLA"
     response.code.to_i.should == 404
   end
 
-  #TODO revisit
   it "should return 501 for unknown processing instruction" do
-    querystring = "BAD_NORM?location=file:///Users/Carol/Workspace/describe/files/GLASS.WAV"
-    response = send("GET", querystring)
-    puts response.body
+    get "transform/BAD_NORM?location=file:///Users/Carol/Workspace/describe/files/GLASS.WAV"
     response.code.to_i.should == 501
   end
 
   it "should return 500 for transformation failure" do
-    querystring = "WAVE_NORM?location=file:///Users/Carol/Workspace/describe/files/etd.pdf"
-    response = send("GET", querystring)
-    puts response.body
+    get "transform/WAVE_NORM?location=file:///Users/Carol/Workspace/describe/files/etd.pdf"
     response.code.to_i.should == 500
-  end
-
-  #todo: implement put
-  it "it should return 405 for PUT" do  
-    response = send("PUT", nil, nil)
-    puts response.body
-    response.code.to_i.should == 405
-  end
-
-  it "should return 405 for unsupported DELETE method" do
-    response = send("DELETE")
-    puts response.body
-    response.code.to_i.should == 405
-  end
-
-  it "should return 405 for unsupported POST method" do
-    response = send("POST")
-    puts response.body
-    response.code.to_i.should == 405
-  end
-
-  #todo: may implement HEAD to return agent information
-  it "should return 405 for unsupported HEAD method" do
-    response = send("HEAD")
-    puts response.body
-    response.code.to_i.should == 405
-  end
-
-  it "should return 405 for bogus method" do
-    response = send("TEST")
-    puts response.body
-    response.code.to_i.should == 405
-  end
-
-  it "should return 505 if not in HTTP/1.1" do   
-    response = send("GET", nil, nil, { 'Version' => 'HTTP/1.0' })
-    response.code.to_i.should == 505
   end
 
 end
